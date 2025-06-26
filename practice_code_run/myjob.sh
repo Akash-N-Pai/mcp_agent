797f7b3eb5155eec843ade9b683ef2bcd39405aa
Akash <akashvnp@login03.af.uchicago.edu>
2025-06-26 14:26:12 -0500

    submited a mock job

--- practice_code_run/myjob.sh
+++ practice_code_run/myjob.sh
@@ -2,14 +2,16 @@
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 export ALRB_localConfigDir=$HOME/localConfig
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
-
 lsetup rucio
 rucio --verbose download --rse MWT2_DATADISK data16_13TeV:AOD.11071822._001488.pool.root.1
 
+# You can run things like asetup as well
 asetup AnalysisBase,21.2.81
 
+# This is where you would do your data analysis via AnalysisBase, etc. We will
+# just pretend to do that, and truncate the file to simulate generating an
+# output file. This is definitely not what you want to do in a real analysis!
 cd data16_13TeV
 truncate --size 10MB AOD.11071822._001488.pool.root.1
 cp AOD.11071822._001488.pool.root.1 $DATA/myjob.output
-
 sleep 300
